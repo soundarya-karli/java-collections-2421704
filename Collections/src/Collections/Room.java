0dbf21430b2ad364957985644b13cfe9f3606e83
Soundarya <67725756+soundarya-karli@users.noreply.github.com>
2023-09-29 08:56:14 +0000

    room

---
+++ Collections/src/Collections/Room.java
@@ -0,0 +1,72 @@
+package Collections;
+
+public class Room {
+    private String name;
+    private String type;
+    private int capacity;
+    private double rate;
+    public Room(String name, String type, int capacity, double rate){
+        this.name=name;
+        this.type=type;
+        this.capacity=capacity;
+        this.rate=rate;
+    }
+    
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return int return the capacity
+     */
+    public int getCapacity() {
+        return capacity;
+    }
+
+    /**
+     * @param capacity the capacity to set
+     */
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    /**
+     * @return double return the rate
+     */
+    public double getRate() {
+        return rate;
+    }
+
+    /**
+     * @param rate the rate to set
+     */
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+}
