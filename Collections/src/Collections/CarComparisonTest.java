92d5ac9d9da37620da3c1a81a666b9806520e176
Soundarya <67725756+soundarya-karli@users.noreply.github.com>
2023-10-04 09:39:04 +0000

    car tests

--- Collections/src/Collections/CarComparisonTest.java
+++ Collections/src/Collections/CarComparisonTest.java
@@ -1,11 +1,9 @@
 package Collections;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.util.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class CarComparisonTest {
     Collection <Car> cars;
@@ -14,5 +12,35 @@ public class CarComparisonTest {
     @BeforeEach
     public void setUp(TestInfo info) throws Exception{
         System.out.format("%nPerforming %s%n", info.getTestMethod().get().getName());
+        this.cars=new ArrayList<>();
+        this.honda=new Car("Honda","A",40500);
+        this.tesla=new Car("Tesla","X",35000);
+        this.tata=new Car("TATA","S",25000);
+        this.cars.addAll(Arrays.asList(honda,tesla,tata));
+    }
+    @AfterEach
+    public void tearDown() throws Exception{
+        System.out.format("%nResults%n");
+        this.cars.stream().
+                map(c->String.format("%s %s",c.getMake(),c.getModel())).
+                forEach(System.out::println);
+    }
+
+    @Test
+    public void removeTest(){
+        this.cars.remove(honda);
+        this.cars.remove(new Car("TATA","S",25000));
+        assertEquals(1,this.cars.size());
+    }
+    @Test
+    public void containsTest(){
+        assertTrue(cars.contains(new Car("Honda","A",40500)));
+        assertTrue(cars.contains(tesla));
+        assertTrue(cars.contains(tata));
+    }
+    @Test
+    public void removeAllTest(){
+        this.cars.removeAll(Arrays.asList(honda,tesla,new Car("TATA","S",25000))) ;
+        assertTrue(cars.isEmpty());               
     }
 }
