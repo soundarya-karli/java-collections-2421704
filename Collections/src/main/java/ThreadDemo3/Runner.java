62ef99ec98573808504fc2d3060977533627f96c
Soundarya <67725756+soundarya-karli@users.noreply.github.com>
2023-10-12 09:50:42 +0000

    deadlock

--- Collections/src/main/java/ThreadDemo3/Runner.java
+++ Collections/src/main/java/ThreadDemo3/Runner.java
@@ -8,12 +8,30 @@ public class Runner {
        Account a2=new Account();
        private Lock lock1=new ReentrantLock();
        private Lock lock2=new ReentrantLock();
+       private void acquireLocks(Lock l1, Lock l2){
+        while(true){
+            boolean gotl1=false;
+            boolean gotl2=false;
+            try{
+                gotl1=l1.tryLock();
+                gotl2=l2.tryLock();
+            }
+            finally{
+                if(gotl1 && gotl2) return;
+                if(gotl1){
+                    l1.unlock();
+                }
+                if(gotl2){
+                    l2.unlock();
+                }
+            }
+        }
+       }
 
     public void first(){
       Random random=new Random();
       for(int i=0;i<1000;i++){
-        lock1.lock();
-        lock2.lock();
+        acquireLocks(lock1, lock2);
         try{
       Account.transfer(a1,a2,random.nextInt(100));}
       finally{
@@ -25,8 +43,8 @@ public class Runner {
     public void second(){
          Random random=new Random();
       for(int i=0;i<1000;i++){
-      lock1.lock();
-        lock2.lock();
+        acquireLocks(lock2, lock1);
+      
         try{
       Account.transfer(a2,a1,random.nextInt(100));}
       finally{
@@ -37,7 +55,7 @@ public class Runner {
     }
     public void finished(){
          System.out.println("Account 1 Bal: "+a1.getBalance());
-         System.out.println("Account 2 Bal: "+a1.getBalance());
+         System.out.println("Account 2 Bal: "+a2.getBalance());
          System.out.println("Total Bal: "+(a1.getBalance()+a2.getBalance()));
     }
 }
