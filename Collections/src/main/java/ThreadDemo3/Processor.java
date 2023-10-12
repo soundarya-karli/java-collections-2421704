d047f1c5dca483c3acf9b0e783e31a347b0ba85d
Soundarya <67725756+soundarya-karli@users.noreply.github.com>
2023-10-12 09:05:45 +0000

    wait notify lock

--- Collections/src/main/java/ThreadDemo3/Processor.java
+++ Collections/src/main/java/ThreadDemo3/Processor.java
@@ -1,26 +1,45 @@
 package ThreadDemo3;
 
+import java.util.LinkedList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Processor {
+    LinkedList<Integer> list=new LinkedList<>();
+    int LIMIT=10;
+    Object lock=new Object();
     public void producer() throws InterruptedException{
-        synchronized(this){
+        int value=0;
+        while(true){
+        synchronized(lock){
+            while(list.size()==LIMIT){
+                lock.wait();
+            }
             System.out.println("Started Producer..");
-            wait();
-            System.out.println("Resume Producer");
+            list.add(value++);
+            lock.notify();  
         }
-
+    }
     }
     public void consumes() throws InterruptedException{
-        Scanner sc=new Scanner(System.in);
-        Thread.sleep(3000);
-        synchronized(this){
-            System.out.println("Entry return key..");
-            sc.nextLine();
-            System.out.println("Return key pressed");
-            notify();
-            Thread.sleep(5000);
+        Random r=new Random();
+       // Scanner sc=new Scanner(System.in);
+        //Thread.sleep(3000);
+        while(true){
+        synchronized(lock){
+           // System.out.println("Entry return key..");
+           // sc.nextLine();
+           // System.out.println("Return key pressed");
+           while(list.size()==0){
+            lock.wait();
+           }
+           System.out.println("Started Consumer..");
+           System.out.print("Size:"+list.size());
+           int val=list.removeFirst();
+           System.out.println(";Value:"+val);
+           lock.notify();
+            Thread.sleep(r.nextInt(2000));
         }
-
+    }
     }
 }
