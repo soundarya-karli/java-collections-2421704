0acf110b6fcfed4571216978ddeca2b3befc64ae
Soundarya <67725756+soundarya-karli@users.noreply.github.com>
2023-10-16 11:12:25 +0000

    fos

--- Collections/src/main/java/IOStream/App.java
+++ Collections/src/main/java/IOStream/App.java
@@ -1,27 +1,41 @@
 package IOStream;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.*;
 
 public class App {
     public static void main(String args[]){
         FileInputStream fs=null;
+        FileOutputStream fos=null;
         try {
             fs=new FileInputStream("/workspaces/java-collections-2421704/Collections/src/main/java/testSample.txt");
+            fos=new FileOutputStream("/workspaces/java-collections-2421704/Collections/src/main/java/testCopy.txt");
             System.out.println("File opened!");
             int i;
             while((i=fs.read())!=-1){
                 System.out.print((char)i);
+                fos.write(i);
             }
+           
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
+         
             e.printStackTrace();
         }
      catch (IOException e) {
-    // TODO Auto-generated catch block
+
     e.printStackTrace();
-}    
+}
+finally{ 
+    try {
+        fs.close();
+        fos.close();
+    } catch (IOException e) {
+
+        e.printStackTrace();
+    }
+}
+System.out.println("File closed");    
 }
     
 }
